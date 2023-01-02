2be4fbe3cebb010ee5fd58788c95db8cdb2b3896
Matias Gonzalez <mng_epson@hotmail.com>
2023-01-02 09:43:09 -0300

    fix: resolve updateCharacters

--- controllers/characters.controller.js
+++ controllers/characters.controller.js
@@ -27,13 +27,18 @@ export const getOneCharacter = async (req, res)=>{
 //**********************Post*******************/
 //POST to table characters
 export const postCharacters = async (req, res)=> {
-  const {name, lastName} = req.body
+  const {name, lastName, origin, position, crew, reward} = req.body
 
   try {
-  const [rows] = await pool.query('INSERT INTO Characters (name, last_name) VALUES (?,?)', [name, lastName])
+  const [rows] = await pool.query('INSERT INTO Characters (name, last_name, origin, position, crew, reward) VALUES (?,?,?,?,?,?)', [name, lastName, origin, position, crew, reward])
   res.send({
     id: rows.insertId,
-    name
+    name,
+    lastName,
+    origin,
+    position,
+    crew,
+    reward
   })
   }catch (error){
     return res.status(500).json({
@@ -46,15 +51,18 @@ export const postCharacters = async (req, res)=> {
 export const pathCharacters = async (req, res)=> {
   //las llaves son el equivalente a id = req.params.id
   const {id} = req.params
-  const {name, lastName} = req.body
+  const {name, lastName, origin, position, crew, reward} = req.body
   try {
   
   //en el caso de querer cambiar solo un dato se debe cambiar el name = ? por name = IFNULL(?, name)
-  const result = await pool.query('UPDATE Characters SET (name, last_name) VALUES (?,?) WHERE id_character = ?', [name, lastName, id])
+  const result = await pool.query('UPDATE Characters SET name = IFNULL(?, name), last_name = IFNULL(?, last_name), origin = IFNULL(?, origin), position = IFNULL(?, position), crew =IFNULL(?, crew), reward = IFNULL(?, reward)  WHERE id_character = ?', [name, lastName, origin, position, crew, reward, id])
 
   if (result.affectedRows == 0) return res.status(404).json({
     message: 'Character not found'
   })
+  else if (result.affectedRows == 1) return res.status(200).json({
+    message: 'Character modified'
+  })  
 
   const [rows]= await pool.query('SELECT * FROM Characters WHERE id_character = ?', [id])
 
