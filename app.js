2431a3765480a9a26db2d9bf0e666ac4dcc565f1
Matias Gonzalez <mng_epson@hotmail.com>
2023-01-05 08:55:17 -0300

    feat: add init route and views folder

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 
 import express from "express";
 import charactersRoutes from './routes/characters.routes.js';
-import indexRoutes from './routes/index.routes.js'
+// import indexRoutes from './routes/index.routes.js'
 import cors from "cors";
-import { ping } from "./controllers/index.controller.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import * as path from 'path';
+// import { ping } from "./controllers/index.controller.js";
 
 
 const app = express();
@@ -12,11 +15,17 @@ const app = express();
 app.use(cors());
 app.use(express.json())
 
-//Conexion a la db
-app.use(indexRoutes)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-//Rutas
-// app.use('/', ping)
+//Conexion a la db
+// app.use(indexRoutes)
+
+app.get('/', (req,res)=> {
+  let html = path.resolve(__dirname, './views/home.html')
+  res.sendFile(html)
+})
+
 
 app.use('/api',charactersRoutes)
 
--- controllers/index.controller.js
+++
@@ -1,4 +0,0 @@
-
-export const ping = async (req,res)=> {
-  res.send('Welcome to the ONE API for more info try copy and paste at final of navigator the next url /api/characters and use free')
-}
